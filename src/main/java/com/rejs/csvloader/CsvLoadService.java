39599fd612d2bf5e4c4aaa2999b9e7a55fcb44f8
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-03-17 18:14:09 +0900

    feat: CsvLoadService

---
+++ src/main/java/com/rejs/csvloader/CsvLoadService.java
@@ -0,0 +1,47 @@
+package com.rejs.csvloader;
+
+import com.rejs.csvloader.file.FileSystemAccessObject;
+import com.rejs.csvloader.file.LocalFileSystemAccessObject;
+import com.rejs.csvloader.repository.JdbcBatchInsertRepository;
+import com.rejs.csvloader.repository.JdbcTemplateProvider;
+import com.rejs.csvloader.repository.query.InsertQueryBuilder;
+import com.rejs.csvloader.validator.CsvColumnValidateService;
+import com.rejs.csvloader.validator.CsvColumnValidationResult;
+import com.rejs.csvloader.yaml.ImportPropertiesLoader;
+import com.rejs.csvloader.yaml.properties.config.ImportProperties;
+import com.rejs.csvloader.yaml.properties.model.ImportColumn;
+import com.rejs.csvloader.yaml.properties.model.ImportWork;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class CsvLoadService {
+    private final InsertQueryBuilder insertQueryBuilder;
+    private final ImportPropertiesLoader loader;
+    private final FileSystemAccessObject fao = new LocalFileSystemAccessObject();
+    private final CsvColumnValidateService validateService;
+    private final JdbcBatchInsertRepository jdbcBatchInsertRepository;
+    private final JdbcTemplateProvider jdbcTemplateProvider;
+
+    public void load(String yml, String csv){
+        ImportProperties properties = loader.loadProperties(yml);
+        Resource data = fao.load(csv);
+        JdbcTemplate jdbcTemplate = jdbcTemplateProvider.createJdbcTemplate(properties.getDatabase());
+
+        for(ImportWork work : properties.getWorks()){
+            String query = insertQueryBuilder.buildInsertQuery(work);
+
+            List<ImportColumn> columns = work.getColumns();
+            CsvColumnValidationResult result = validateService.validate(columns, data);
+
+            List<Object[]> datas = result.getValidDatas();
+            jdbcBatchInsertRepository.batchInsert(jdbcTemplate, query, datas, columns);
+        }
+    }
+
+}
