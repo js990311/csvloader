a0b089dbc70f31308444f0df14c8344e95c76d45
Lee jong sik <102013392+js990311@users.noreply.github.com>
2025-03-22 15:14:08 +0900

    feat: DoubleValidator

---
+++ src/main/java/com/rejs/csvloader/Validator/impl/DoubleValidator.java
@@ -0,0 +1,38 @@
+package com.rejs.csvloader.validator.impl;
+
+import com.rejs.csvloader.validator.CsvColumnValidator;
+import com.rejs.csvloader.validator.exception.InvalidCsvColumnException;
+import com.rejs.csvloader.yaml.properties.model.ColumnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DoubleValidator implements CsvColumnValidator{
+
+    @Override
+    public Object validate(String column, ColumnProperty property) {
+        try {
+            Double number = Double.parseDouble(column);
+            if(property.getConstraints() != null){
+                if(property.getConstraints().getMax() != null && number>property.getConstraints().getMax()){
+                    throw new InvalidCsvColumnException("invalid number range");
+                }
+                if(property.getConstraints().getMin() != null && number<property.getConstraints().getMin()){
+                    throw new InvalidCsvColumnException("invalid number range");
+                }
+            }
+            return number;
+        }catch (NumberFormatException e){
+            if(property.isNullable() && column == null){
+                return null;
+            }
+            throw new InvalidCsvColumnException("column is not number");
+        }catch (RuntimeException exception){
+            throw new InvalidCsvColumnException(exception);
+        }
+    }
+
+    @Override
+    public boolean support(String type) {
+        return "Double".equals(type);
+    }
+}
